8311b0dabaf2708f1bec832e41639325eb620ef9
Olga.Lavrichenko <olga.lavrichenko@jetbrains.com>
2026-04-06 08:57:32 +0200

    Check out stable branch during install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -e
 REPO_URL="https://github.com/JetBrains/bonsai.git"
 REPO_DIR="bonsai"
 
-echo "Cloning $REPO_URL..."
-git clone "$REPO_URL" "$REPO_DIR"
+echo "Cloning $REPO_URL (stable branch)..."
+git clone --branch stable "$REPO_URL" "$REPO_DIR"
 
 cd "$REPO_DIR"
 
