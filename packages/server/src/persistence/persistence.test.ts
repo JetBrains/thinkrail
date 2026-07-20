34f352de3de2da3c60303d922caa5c005fcf718d
danyaberezun <danya.berezun@gmail.com>
2026-07-20 20:58:34 +0200

    feat(persistence): add hook overrides + approvals storage

---
+++ packages/server/src/persistence/persistence.test.ts
@@ -0,0 +1,42 @@
+import { afterEach, beforeEach, expect, test } from "bun:test";
+import { mkdtempSync, rmSync } from "node:fs";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+import {
+	loadHookApprovals,
+	loadHookOverrides,
+	saveHookApprovals,
+	saveHookOverrides,
+} from "./persistence";
+
+let dataDir: string;
+const savedDataDir = process.env.THINKRAIL_DATA_DIR;
+
+beforeEach(() => {
+	dataDir = mkdtempSync(join(tmpdir(), "trpi-persistence-test-"));
+	process.env.THINKRAIL_DATA_DIR = dataDir;
+});
+
+afterEach(() => {
+	rmSync(dataDir, { recursive: true, force: true });
+	if (savedDataDir === undefined) delete process.env.THINKRAIL_DATA_DIR;
+	else process.env.THINKRAIL_DATA_DIR = savedDataDir;
+});
+
+test("loadHookOverrides defaults to an empty object when the file doesn't exist", () => {
+	expect(loadHookOverrides()).toEqual({});
+});
+
+test("saveHookOverrides then loadHookOverrides round-trips", () => {
+	saveHookOverrides({ p1: { onCreate: "pnpm install" } });
+	expect(loadHookOverrides()).toEqual({ p1: { onCreate: "pnpm install" } });
+});
+
+test("loadHookApprovals defaults to an empty object when the file doesn't exist", () => {
+	expect(loadHookApprovals()).toEqual({});
+});
+
+test("saveHookApprovals then loadHookApprovals round-trips", () => {
+	saveHookApprovals({ p1: { onCreate: "abc123hash" } });
+	expect(loadHookApprovals()).toEqual({ p1: { onCreate: "abc123hash" } });
+});
